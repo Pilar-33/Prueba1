1fb0bc00bea13398fa6c713b43153d9aa8f66949
Pilar Levano <violinadrian08@gmail.com>
2024-01-08 20:56:40 -0300

    Nueva línea de codigo2

--- vector.py
+++ vector.py
@@ -1,3 +1,4 @@
-print("La suma de: 2 + 4 = "+str(2+4))
-print("La resta de: 15 - 4 ="+str(15-4))
+print("La suma de: 2 + 4 = " + str(2 + 4))
+print("La resta de: 15 - 4 =" + str(15 - 4))
 print("Hola mundo")
+print("La multiplicación de: 2*8 = " + str(2 * 8))
